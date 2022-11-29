a1ac2a988074b5d9b92de18275f1eb616e1fabfb
OleksandrZhytin <oleksandr.zhytin@betterme.world>
2022-11-29 16:33:37 +0200

    ArithmeticAverage task

---
+++ src/main/java/HomeTasks/ArithmeticAverage.java
@@ -0,0 +1,23 @@
+package HomeTasks;
+
+
+import java.util.Scanner;
+
+public class ArithmeticAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите первое число:");
+        int a = sc.nextInt();
+
+        System.out.println("Введите второе число:");
+        int b = sc.nextInt();
+
+        System.out.println("Введите третее число:");
+        int c = sc.nextInt();
+
+        double sum = a + b + c;
+        double avg = sum / 3;
+        System.out.println(avg);
+    }
+}

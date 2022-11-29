54f2ee5060c7583f92e705c5339e8d6b3afbb192
OleksandrZhytin <oleksandr.zhytin@betterme.world>
2022-11-29 19:18:09 +0200

    "3 variables" task

---
+++ src/main/java/HomeTasks/Task_3_Beginner.java
@@ -0,0 +1,13 @@
+package HomeTasks;
+
+public class Task_3_Beginner {
+    public static void main(String[] args) {
+        int x = 10, y = 12, z = 3;
+
+        System.out.println(x + y - ((x + 1) * z));
+        System.out.println((x - 1) - y * 5);
+        System.out.println(y / x + 5 % z);
+        System.out.println((x + 1) + y * 5);
+        System.out.println(y-((x+1)*z));
+    }
+}

1f5ff860e883916aea2cf5e0dd399a7f06ae5325
AlexZhitin <aszhitin@gmail.com>
2023-01-15 19:42:15 +0200

    Fixed CustomerArray

--- src/main/java/HomeTasks/CustomerArray.java
+++ src/main/java/HomeTasks/CustomerArray.java
@@ -1,5 +1,6 @@
 package HomeTasks;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CustomerArray {
@@ -10,12 +11,14 @@ public class CustomerArray {
  вывести все нечетные значения.
 */
 
-    static void createArray(int[] array, int n) {
+    static int[] createArray(int n) {
         System.out.print("Все числа в массиве: ");
-        for (int i = 0; i < array.length; i++) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
             array[i] = (int) ((Math.random() * n));
             System.out.print(array[i] + " ");
         }
+        return array;
     }
 
     static int minNumber(int[] array) {
@@ -44,35 +47,44 @@ public class CustomerArray {
         return sum;
     }
 
-    static int avgNumber(int[] array, int n) {
+    static int avgNumber(int[] array) {
         int sum = 0;
         for (int i : array) {
             sum = sum + i;
         }
-        return sum / n;
+        return sum / array.length;
     }
 
-    static void oddNumber(int[] array) {
+    static int[] oddNumbers(int[] array) {
+        int sum = 0;
+        for (int number : array) {
+            if (number % 2 == 1) {
+                sum++;
+            }
+        }
+        int[] arrayOfOddNumbers = new int[sum];
+        int index = 0;
 
         for (int i : array) {
-            if (i % 2 != 0)
-                System.out.print(i + " ");
+            if (i % 2 == 1) {
+                arrayOfOddNumbers[index] = i;
+                index++;
+            }
         }
+        return arrayOfOddNumbers;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введи количество элементов в массиве: ");
         int n = sc.nextInt();
-        int[] array = new int[n];
-        createArray(array, n);
+        int[] array = createArray(n);
 
         System.out.print("\n" + "Наименьшее число: " + minNumber(array));
         System.out.print("\n" + "Наибольшее число: " + maxNumber(array));
         System.out.print("\n" + "Сумма чисел: " + sumNumber(array));
-        System.out.print("\n" + "Среднее арифметическое: " + avgNumber(array, n));
-        System.out.print("\n" + "Нечетные числа: ");
-        oddNumber(array);
+        System.out.print("\n" + "Среднее арифметическое: " + avgNumber(array));
+        System.out.print("\n" + "Нечетные числа: " + Arrays.toString(oddNumbers(array)));
     }
 }
 

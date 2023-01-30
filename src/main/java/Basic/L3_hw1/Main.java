3cdeed104fcbafaf38316d6eb86777dcce285d55
AlexZhitin <aszhitin@gmail.com>
2023-01-30 12:43:02 +0200

    L3_hw1

---
+++ src/main/java/Basic/L3_hw1/Main.java
@@ -0,0 +1,14 @@
+package Basic.L3_hw1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Printer p = new Printer();
+        Printer p2 = new Derivative();
+        Printer p3 = new Derivative();
+
+        p.print("text");
+        p2.print("text");
+        p3.print("text");
+    }
+}

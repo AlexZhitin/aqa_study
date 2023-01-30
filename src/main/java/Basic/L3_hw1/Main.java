747f22424407df24dee4eeb70fe21dcc76209cae
AlexZhitin <aszhitin@gmail.com>
2023-01-30 12:45:20 +0200

    L3_hw1

--- src/main/java/Basic/L3_hw1/Main.java
+++ src/main/java/Basic/L3_hw1/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Printer p = new Printer();
         Printer p2 = new Derivative();
-        Printer p3 = new Derivative();
+        Printer p3 = new Derivative2();
 
         p.print("text");
         p2.print("text");

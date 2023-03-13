3dfbd093622f7501c954cba82d91a1b597f16969
AlexZhitin <aszhitin@gmail.com>
2023-03-13 20:23:43 +0200

    L5_hw1

---
+++ src/main/java/Basic/L5_hw1/Main.java
@@ -0,0 +1,36 @@
+package Basic.L5_hw1;
+
+/*Создать класс Animal с полями type и name
+Cоздать класс Zoo. В классе создать конструктор который принимает список животных.
+Создать методы для добавления нового животного addAnimal(String name, String type),  удаления животного по его name deleteAnimal(String animalName) и метод для вывода всего зоопарка в консоль.
+
+Создать метод который выводит отсортированый список животных в формате {type, name}*/
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Zoo zoo1 = new Zoo(new ArrayList<>());
+        zoo1.addAnimal("Elephant", "Mammal");
+        zoo1.addAnimal("Giraffe", "Mammal");
+        zoo1.addAnimal("Lion", "Mammal");
+
+        System.out.println("__________________________________________________");
+        zoo1.printAnimals();
+
+        System.out.println("___");
+
+        zoo1.deleteAnimal("Lion");
+        zoo1.printAnimals();
+
+        System.out.println("__________________________________________________");
+
+        Zoo zoo2 = new Zoo(new Animal("Mark", "Stork"), new Animal("Zhora", "Beaver"), new Animal("Pedro", "Dog"));
+        zoo2.printAnimals();
+
+        System.out.println("___");
+
+        zoo2.deleteAnimal("Pedro");
+        zoo2.printAnimals();
+    }
+}
---
+++ src/main/java/Basic/L5_hw1/Zoo.java
@@ -0,0 +1,31 @@
+package Basic.L5_hw1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Zoo {
+    private final ArrayList<Animal> animals;
+
+    public Zoo(ArrayList<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public Zoo(Animal... animals) {
+        this.animals = new ArrayList<>(Arrays.asList(animals));
+    }
+
+    public void addAnimal(String name, String type) {
+        Animal newAnimal = new Animal(name, type);
+        animals.add(newAnimal);
+    }
+
+    public void deleteAnimal(String name) {
+        animals.removeIf(animal -> animal.getName().equals(name));
+    }
+
+    public void printAnimals() {
+        for (Animal animal : animals) {
+            System.out.println(animal.getName() + " - " + animal.getType());
+        }
+    }
+}

d094c9bd05acaa0c0eb0050f4f2f4973fbfd5cce
AlexZhitin <aszhitin@gmail.com>
2023-05-15 13:17:56 +0300

    L7_hw1

--- src/main/java/Basic/L7_hw1/Price.java
+++ src/main/java/Basic/L7_hw1/Price.java
@@ -1,2 +1,37 @@
-package Basic.L7_hw1;public class Price {
+package Basic.L7_hw1;
+
+public class Price {
+    private String product;
+    private String store;
+    private int price;
+
+    public Price(String product, String store, int price) {
+        this.product = product;
+        this.store = store;
+        this.price = price;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public void setStore(String store) {
+        this.store = store;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }

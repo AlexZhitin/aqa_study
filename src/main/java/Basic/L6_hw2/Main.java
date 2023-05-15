dd20b7498bce703ecd55e9826050563b11acc435
AlexZhitin <aszhitin@gmail.com>
2023-05-15 21:26:13 +0300

    Tiny fixes

--- src/main/java/Basic/L6_hw2/Main.java
+++ src/main/java/Basic/L6_hw2/Main.java
@@ -5,16 +5,9 @@ package Basic.L6_hw2;
 // Подсказка используйте библеотеку Faker для наполнения книги значением.
 // Вывидете на экран содержимое библиотеки в формате Map состоящая из параметров name и writer
 
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
         Library library = new Library(2);
-
-//        for (Map.Entry<String, String> libraryEntry : library.getLibrary().entrySet()) {
-//            System.out.println( "\"" + libraryEntry.getKey() +"\"" + " by " + libraryEntry.getValue());
-//        }
-
         library.getLibrary().forEach((libraryKey, libraryValue) -> System.out.println( "\"" + libraryKey +"\"" + " by " + libraryValue));
     }
 }
